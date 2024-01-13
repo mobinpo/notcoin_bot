64174095b81e813665c578b2888af6ecf312d6ab
mobinpo <40354181+mobinpo@users.noreply.github.com>
2024-01-13 18:01:18 +0330

    Update on_startup.py

--- database/on_startup.py
+++ database/on_startup.py
@@ -1,8 +1,9 @@
 import aiosqlite
 
-
 async def on_startup_database() -> None:
+    # برقراری اتصال به پایگاه داده
     async with aiosqlite.connect(database='database/sessions.db') as db:
+        # اجرای دستور SQL برای ایجاد جدول در صورت عدم وجود
         await db.execute(sql="""
         CREATE TABLE IF NOT EXISTS sessions (
             id INTEGER PRIMARY KEY,
@@ -10,4 +11,5 @@ async def on_startup_database() -> None:
             session_proxy TEXT
         );
         """)
+        # ذخیره تغییرات در پایگاه داده
         await db.commit()
